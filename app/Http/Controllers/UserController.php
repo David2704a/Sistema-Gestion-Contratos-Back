dc1409d30ac9d687d8fb09fe51073784dc02e4f1
Gestion Tecnologica Ags Salud 1 <gestiontecnologica1agssalud@gmail.com>
2024-11-19 14:15:45 -0500

    cambios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getUser(){
+        $user = DB::table('users')->get();
+
+        return response()->json($user);
+    }
 }
