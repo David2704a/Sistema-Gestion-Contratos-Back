50b2c630d94d2842e10c01b639e3873f60050d57
JohanB <davidbm270@gmail.com>
2024-11-19 14:09:37 -0500

    Creacon del Controlador de Auth y la Api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => $user
+            ], 200);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401); 
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/*
+=====================================
+API LOGIN
+=====================================
+*/
+Route::get('/prueba', function () {
+    return 'Esta es una prueba';
+});
+
+Route::post('/login', [AuthController::class, 'login']);
