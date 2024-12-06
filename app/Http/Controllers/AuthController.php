775b8fac3fdbb8e1213d5c7712ffc1dab8a6b54b
L0P3ZS <auxhistoriaclinica@agssalud.com>
2024-12-06 16:19:36 -0500

    registro de usuarios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Services\AuthService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB ;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -19,6 +22,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
  
+        // dd( $request->input('email'), $request->input('password') );
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
@@ -35,8 +39,30 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Inicio de sesion exitoso',
             'token' => $token,
-            'token_type' => 'Bearer',
+            'token_type' => 'Bearer'
         ]);
     
+    }
+
+
+    public function register(Request $request){
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $name = $request->input('name');
+
+        $datos = DB::table('users')->insert([
+            'name' => $name,
+            'email' => $email,
+            'id_persona' => 3,
+            'password' => Hash::make($password)
+
+        ]);
+
+        if($datos){
+            return response()->json(['message' => 'Usuario creado exitosamente'], 201);
+        }else{
+            return response()->json(['message' => 'Error al crear el usuario'], 500);
+        }
+       
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ API LOGIN
 
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
 
 /*
 =====================================
